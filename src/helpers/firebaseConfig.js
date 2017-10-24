65d070a01cfbbe7526603fba61fb19b3cb6f6ac6
lucasbrunoferreira <lucasbrunoferreira@gmail.com>
2017-10-24 00:10:22 -0200

    Reconstrução com Materialize

--- src/helpers/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-  export const config = {
-    apiKey: 'AIzaSyCfkZk6TzqVMynIiIITTrAvQnGBoPEnyz4',
-    authDomain: 'projeto-tickets.firebaseapp.com',
-    databaseURL: 'https://projeto-tickets.firebaseio.com',
-    projectId: 'projeto-tickets',
-    storageBucket: 'projeto-tickets.appspot.com',
-    messagingSenderId: '138822530180'
-  }
