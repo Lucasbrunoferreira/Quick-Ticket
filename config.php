eef0b0bd4bfa890ed02cd9c29aa30457afa66e4c
Rodriguess <viniciusrdefreitas@gmail.com>
2017-10-28 18:57:51 -0200

    autenticação + banco de dados

---
+++ config.php
@@ -0,0 +1,5 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<?php
+  $coneccao = @mysql_connect("localhost", "root", "") or die("Não foi possível conectar com os servidores.");
+  mysql_select_db("QuickTicket", $coneccao) or die("Banco de Dados não encontrado.");
+?>
---
+++ inicio.php
@@ -0,0 +1,5 @@
+<?php
+session_start();
+echo "bem vindo <strong>".$_session['nome'];
+
+ ?>
